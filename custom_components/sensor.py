375b9a43ad8a577efc7297741d0409380b50e627
Jesse Mäkinen <jesse.j.makinen@gmail.com>
2023-12-05 20:34:05 +0200

    Bugfixes related to data updates

--- custom_components/sensor.py
+++ custom_components/sensor.py
@@ -35,7 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
     ignored_chains = str(entry.data.get(CONF_IGNORED_CHAINS) if not None else '').lower().split(",")
     fuels = str(entry.data.get(CONF_FUELS) if not None else '').lower().split(",")
-    cheapest_limit = entry.data.get(CONF_CHEAPEST_LIMIT)
+    cheapest_limit = entry.data.get(CONF_CHEAPEST_LIMIT) if not None else 0
 
     cheapest = {}
 
@@ -62,13 +62,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
                 if price is not None:
                     updated = datetime.fromisoformat(str(price["updated"]).removesuffix('Z')).replace(
                         tzinfo=datetime.now().astimezone().tzinfo)
-                    if cheapest_limit == 0 or cheapest_limit is None or (
-                            datetime.now(updated.tzinfo) - updated).total_seconds() < (cheapest_limit * 60):
+                    if cheapest_limit == 0 or (datetime.now(updated.tzinfo) - updated).total_seconds() < (
+                            cheapest_limit * 60):
                         cheapest[fuel_type].append({ATTR_PRICE: price["price"], ATTR_STATION: station["name"],
                                                     ATTR_UPDATED: updated})
 
     def sort_price(val: dict):
-        return val["price"]
+        return val[ATTR_PRICE]
 
     for fuel_type in FUEL_TYPES:
         if len(fuels) == 0 or fuel_type in fuels:
@@ -76,7 +76,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
             entities.append(
                 CheapestSensor(coordinator, fuel_type,
                                cheapest[fuel_type] if len(cheapest[fuel_type]) < 5 else cheapest[fuel_type][:5],
-                               ignored_chains))
+                               ignored_chains, cheapest_limit))
 
     async_add_entities(entities)
 
@@ -112,9 +112,16 @@ class TankilleSensor(CoordinatorEntity, SensorEntity):
             if price is not None:
                 self._attr_extra_state_attributes[ATTR_UPDATED] = datetime.fromisoformat(
                     str(price["updated"]).removesuffix('Z')).replace(
-                    tzinfo=datetime.now().astimezone().tzinfo),
-                self._attr_extra_state_attributes[ATTR_PRICE] = str(price["price"]),
-                self.async_write_ha_state()
+                    tzinfo=datetime.now().astimezone().tzinfo)
+                self._attr_extra_state_attributes[ATTR_PRICE] = str(price["price"])
+            else:
+                self._attr_extra_state_attributes[ATTR_UPDATED] = None
+                self._attr_extra_state_attributes[ATTR_PRICE] = None
+        else:
+            self._attr_extra_state_attributes[ATTR_UPDATED] = None
+            self._attr_extra_state_attributes[ATTR_PRICE] = None
+
+        self.async_write_ha_state()
 
     @property
     def native_value(self):
@@ -122,7 +129,8 @@ class TankilleSensor(CoordinatorEntity, SensorEntity):
 
 
 class CheapestSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator: DataUpdateCoordinator, fuel_type: str, data: list, ignored_chains: list[str]):
+    def __init__(self, coordinator: DataUpdateCoordinator, fuel_type: str, data: list, ignored_chains: list[str],
+                 cheapest_limit: int):
         super().__init__(coordinator)
         self._attr_attribution = ATTRIBUTION
         self._attr_icon = "mdi:currency-eur"
@@ -132,17 +140,24 @@ class CheapestSensor(CoordinatorEntity, SensorEntity):
         self._attr_name = f"{coordinator.config_entry.data[CONF_LABEL]} {FUEL_TYPES[fuel_type]}"
         self._fuel_type = fuel_type
         self._ignored_chains = ignored_chains
+        self._cheapest_limit = cheapest_limit
 
         attrs = {
             ATTR_FUEL_TYPE: fuel_type
         }
 
-        for i in range(5):
-            attrs[ATTR_PRICE_ + str(i + 1)] = str(data[i][ATTR_PRICE]) if len(data) > i and data[i][
-                ATTR_PRICE] is not None else None
-            attrs[ATTR_UPDATED_ + str(i + 1)] = data[i][ATTR_UPDATED] if len(data) > i else None
-            attrs[ATTR_STATION_ + str(i + 1)] = data[i][ATTR_STATION] if len(data) > i else None
+        latest_update = None
 
+        for i in range(5):
+            attrs[f"{ATTR_PRICE_}{(i + 1)}"] = str(data[i][ATTR_PRICE]) if len(data) > i and data[i][
+                ATTR_PRICE] is not None else None
+            attrs[f"{ATTR_UPDATED_}{(i + 1)}"] = data[i][ATTR_UPDATED] if len(data) > i else None
+            attrs[f"{ATTR_STATION_}{(i + 1)}"] = data[i][ATTR_STATION] if len(data) > i else None
+
+            if len(data) > i and (latest_update is None or data[i][ATTR_UPDATED] < latest_update):
+                latest_update = data[i][ATTR_UPDATED]
+
+        self._latest_update = latest_update.strftime("%s") if latest_update is not None else None
         self._attr_extra_state_attributes = attrs
 
     @callback
@@ -155,24 +170,37 @@ class CheapestSensor(CoordinatorEntity, SensorEntity):
                 continue
             elif self._fuel_type in station["fuels"]:
                 price = next((x for x in station["price"] if x["tag"] == self._fuel_type), None)
-                data.append({ATTR_PRICE: price["price"], ATTR_STATION: station["name"],
-                             ATTR_UPDATED: datetime.fromisoformat(
-                                 str(price["updated"]).removesuffix('Z')).replace(
-                                 tzinfo=datetime.now().astimezone().tzinfo)})
+                if price is not None:
+                    updated = datetime.fromisoformat(str(price["updated"]).removesuffix('Z')).replace(
+                        tzinfo=datetime.now().astimezone().tzinfo)
+                    if self._cheapest_limit == 0 or (datetime.now(updated.tzinfo) - updated).total_seconds() < (
+                            self._cheapest_limit * 60):
+                        data.append({ATTR_PRICE: str(price["price"]) if price["price"] is not None else None,
+                                     ATTR_STATION: station["name"],
+                                     ATTR_UPDATED: updated})
+
+        latest_update = None
 
         def sort_price(val: dict):
-            return val["price"]
+            return val[ATTR_PRICE]
 
         data.sort(key=sort_price)
-        for i in range(5):
-            self._attr_extra_state_attributes[ATTR_PRICE_ + str(i + 1)] = str(data[i][ATTR_PRICE]) if len(data) > i and \
-                                                                                                      data[i][
-                                                                                                          ATTR_PRICE] is not None else None
-            self._attr_extra_state_attributes[ATTR_UPDATED_ + str(i + 1)] = data[i][ATTR_UPDATED] if len(
-                data) > i else None
-            self._attr_extra_state_attributes[ATTR_STATION_ + str(i + 1)] = data[i][ATTR_STATION] if len(
-                data) > i else None
+        for i in range(1, 5):
+            if len(data) >= i:
+                self._attr_extra_state_attributes[f"{ATTR_PRICE_}{i}"] = data[i - 1][ATTR_PRICE]
+                self._attr_extra_state_attributes[f"{ATTR_UPDATED_}{i}"] = data[i - 1][ATTR_UPDATED]
+                self._attr_extra_state_attributes[f"{ATTR_STATION_}{i}"] = data[i - 1][ATTR_STATION]
+
+                if latest_update is None or data[i - 1][ATTR_UPDATED] < latest_update:
+                    latest_update = data[i - 1][ATTR_UPDATED]
+            else:
+                self._attr_extra_state_attributes[f"{ATTR_PRICE_}{i}"] = None
+                self._attr_extra_state_attributes[f"{ATTR_UPDATED_}{i}"] = None
+                self._attr_extra_state_attributes[f"{ATTR_STATION_}{i}"] = None
+
+        self._latest_update = latest_update.strftime("%s") if latest_update is not None else None
+        self.async_write_ha_state()
 
     @property
     def native_value(self):
-        return self._attr_extra_state_attributes[ATTR_PRICE_ + "1"]
+        return self._latest_update
