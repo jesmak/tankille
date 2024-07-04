5ff961af2c28559509d0545a3026d2f345a315e4
Jesse Mäkinen <jesse.j.makinen@gmail.com>
2024-07-04 18:35:15 +0300

    Fixed a nag about async_forward_entry_setups

--- custom_components/tankille/__init__.py
+++ custom_components/tankille/__init__.py
@@ -38,9 +38,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coord
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
 
